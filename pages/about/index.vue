feb78090b890ceda622d7834b498b2c3748c8d51
Arvind Waghmare <arvind.waghmare@morningstar.com>
2020-12-18 16:50:51 +0530

    first commit

---
+++ pages/about/index.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="about-page">
+        <h1>Welcome to Blog About page</h1>
+    </div>
+</template>
+<style scoped>
+.about-page {
+    text-align: center;
+    padding: 30px;
+}
+</style>
---
+++ pages/admin/_postId/index.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="admin-post-page">
+        <section class="update-form">
+            <AdminPostForm :post="loadedPost" />
+        </section>
+    </div>
+</template>
+<script>
+import AdminPostForm from '@/components/Admin/AdminPostForm';
+export default {
+    components: {
+        AdminPostForm
+    },
+    data() {
+        return {
+            loadedPost: {
+                author: 'Arvin',
+                title: 'Amazing post!',
+                content: 'Super amazing! thanks',
+                thumbnailLink: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4PzgGejsPxVgxwXSsWT3PXX3qB-D-ZTpApw&usqp=CAU'
+            }
+        }
+    }
+}
+</script>
+
+<style scoped>
+.update-form {
+  width: 90%;
+  margin: 20px auto;
+}
+@media (min-width: 768px) {
+  .update-form {
+    width: 500px;
+  }
+}
+</style>
