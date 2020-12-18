928be02121dc2c34cae19d9f647f9b0c6d34e884
Arvind Waghmare <arvind.waghmare@morningstar.com>
2020-12-18 20:06:29 +0530

    adding asyncData to single post

--- pages/index.vue
+++ pages/index.vue
@@ -46,7 +46,7 @@ export default {
     .catch( e => {
       context.error(e);
     })
-  },
+  }
 };
 </script>
 
--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="post-page">
-        <PostList />
+        <PostList :posts="loadedPosts"/>
     </div>
 </template>
 <script>
@@ -9,6 +9,40 @@ export default {
   components: {
     PostList,
   },
+    asyncData(context) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve({
+          loadedPosts: [
+            {
+              id: "1",
+              title: "Amazing post! First",
+              previewText: "Super amazing! thanks - First",
+              thumbnail:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4PzgGejsPxVgxwXSsWT3PXX3qB-D-ZTpApw&usqp=CAU",
+            },
+            {
+              id: "2",
+              title: "Amazing post! Second",
+              previewText: "Super amazing! thanks - Second",
+              thumbnail:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4PzgGejsPxVgxwXSsWT3PXX3qB-D-ZTpApw&usqp=CAU",
+            },
+            {
+              id: "3",
+              title: "Amazing post! Third",
+              previewText: "Super amazing! thanks - Third",
+              thumbnail:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4PzgGejsPxVgxwXSsWT3PXX3qB-D-ZTpApw&usqp=CAU",
+            },
+          ],
+        });
+      }, 1000);
+    })
+    .then( data => {
+      return data;
+    })
+    .catch( e => {
+      context.error(e);
+    })
+  }
 };
 </script>
 <style scoped>
